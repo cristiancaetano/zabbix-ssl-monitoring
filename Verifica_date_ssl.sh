3109cf2677b1b50e3fe0b6cf0007a7b45931efb0
Creative Drive <cristiancaetano@gmail.com>
2019-11-14 14:37:10 -0300

    Colocando Comentarios nas linhas

--- Verifica_date_ssl.sh
+++ Verifica_date_ssl.sh
@@ -1,7 +1,11 @@
 VDATA=`echo | openssl s_client -connect domain.name:443 2>/dev/null | openssl x509 -noout -dates | tail -1 | tr -s " " | cut -d "=" -f2 | cut -d " " -f1,2,4`
+#pega a data que o certificado ira vencer
 
 CONVERTE=$(date -d "$VDATA" +%Y-%m-%d)
+#Converte a data do openssl para o formato correto
 
 DATEATUAL=`date +%Y-%m-%d`
+#verifica a data atual
 
+#calcula as datas e mostra em dias restantes para o certificado vencer
 echo "scale=0;("`date -d "$CONVERTE" +%s`-`date -d "$DATEATUAL" +%s`")"/24/60/60|bc
